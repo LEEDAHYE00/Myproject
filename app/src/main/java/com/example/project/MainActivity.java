cba50d9f77ac85d193cd4009fb9aa3b7629bc7e0
dahye <dlekgp30724@naver.com>
2023-05-09 16:11:08 +0900

    123

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String KEY_ = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
