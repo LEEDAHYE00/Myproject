1ef1c1ccc7140f21f19c69b639b4ebca77c18555
kdavi <kdavin1004@navr.com>
2023-09-27 17:39:01 +0900

    로그인, 회원가입 (넘어가지는 코드 추가 1차)

--- app/src/main/java/com/example/project/activity_login.java
+++ app/src/main/java/com/example/project/activity_login.java
@@ -2,7 +2,10 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_login extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class activity_login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnLogin = (Button) findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity_login.this, activity_mainjoin.class);
+                startActivity(intent);
+            }
+        });
     }
 }
